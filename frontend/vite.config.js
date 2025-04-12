b7b16acbaf0c4abd92507b632943057dd651a374
Arya-kashyap <aryakashyap4188@gmail.com>
2025-04-12 15:41:13 +0530

    vercel commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
     '/api': {
-    target: 'http://localhost:4001',
+    target: 'https://book-store-backend-pink-seven.vercel.app',
     changeOrigin: true,
     rewrite: (path) => path.replace(/^\/api/, ''),
     },
